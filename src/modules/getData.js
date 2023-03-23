50947bd063bd271d5149d4527d73f3550cc7b4aa
LoviMadaan <119787828+LoviMadaan@users.noreply.github.com>
2023-03-24 00:38:00 +0530

    Linters Fixed

--- src/modules/getData.js
+++ src/modules/getData.js
@@ -3,20 +3,20 @@ const gameScore = document.getElementById('game-score');
 const gameScoreBtn = document.getElementById('score-btn');
 
 const myGameUSerData = async () => {
-    await fetch(
-      'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/LoviMadaan1209aaradhya/scores',
-      {
-        method: 'POST',
-        body: JSON.stringify({
-          user: gameUser.value,
-          score: gameScore.value,
-        }),
-        headers: {
-          'Content-type': 'application/json; charset=UTF-8',
-        },
+  await fetch(
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/LoviMadaan1209aaradhya/scores',
+    {
+      method: 'POST',
+      body: JSON.stringify({
+        user: gameUser.value,
+        score: gameScore.value,
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
       },
-    )
-      .then((response) => response.json());
-  };
+    },
+  )
+    .then((response) => response.json());
+};
 
-  gameScoreBtn.addEventListener('click', myGameUSerData)
+gameScoreBtn.addEventListener('click', myGameUSerData);
